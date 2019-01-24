a7c114b0192ff392512ab0987b1d74832ff218cf
Ángel Ordóñez Sánchez <nenust1@outlook.com>
2019-01-23 20:58:47 -0600

    Updates

---
+++ precache-manifest.e7d2d8d3405e4f1bdec333025be8b538.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f740541b9108b912ce54",
+    "url": "/todoreact/static/css/main.c034e82d.chunk.css"
+  },
+  {
+    "revision": "f740541b9108b912ce54",
+    "url": "/todoreact/static/js/main.f740541b.chunk.js"
+  },
+  {
+    "revision": "ab62c889c3922d7deaba",
+    "url": "/todoreact/static/js/1.ab62c889.chunk.js"
+  },
+  {
+    "revision": "c72e28211de6a1967123",
+    "url": "/todoreact/static/js/runtime~main.c72e2821.js"
+  },
+  {
+    "revision": "115bde418d5c033f77c44e7039c31e98",
+    "url": "/todoreact/index.html"
+  }
+];
